b5215b09204e97fb89b372dac6e8c1b3f0474053
Mark Pentler <tehhustler@hotmail.com>
2022-11-27 02:19:25 +0000

    some cosmetic script changes in the stats output

--- scripts/stats.sh
+++ scripts/stats.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i "/Uptime:/{n;s/.*/$(uptime)/}"  /var/www/html/adsbdashboard/index.html
-/usr/bin/echo "***SYSTEM STATS***" >> /tmp/output.ansi
+/usr/bin/echo "***WIFI/UNDERVOLT STATS***" >> /tmp/output.ansi
 /usr/sbin/iwconfig wlan0 | /usr/bin/grep Quality >> /tmp/output.ansi
 /bin/bash /home/pi/throttled.sh >> /tmp/output.ansi
 /usr/bin/ansilove -q -c 50 -o /var/www/html/adsbdashboard/stats.png /tmp/output.ansi
